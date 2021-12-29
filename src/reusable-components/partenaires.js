e7ea949d5ef608211543b1914efb3d6188b49588
CyrielleAlbert <Cyriellealbert@yahoo.fr>
2021-12-29 22:27:27 +0100

    Add partenaire

--- src/reusable-components/partenaires.js
+++ src/reusable-components/partenaires.js
@@ -3,17 +3,20 @@
         <div style={{
             display:"flex",
             flexDirection:"row",
-            justifyContent:"space-around",
+            justifyContent:"normal",
             alignItems: 'center',
-            flexWrap: 'nowrap',
-            overflowX: 'auto',
-            padding: 10,}}>
-            <img src="/Partenaires/Frites-bonnel.png" style={{height:100,width:"auto"}}/>
-            <img src ="/Partenaires/POS.png" style={{height:100,width:"auto"}} />
-            <img src ="/Partenaires/SittiZen.webp" style={{height:100,width:"auto"}} />
-            <img src ="/Partenaires/temps-instant.jpg" style={{height:100,width:"auto"}} />
-            <img src ="/Partenaires/ethernis-drone.jpg" style={{height:100,width:"auto"}} />
-            <img src ="/Partenaires/ludovic-sal.png" style={{height:100,width:"auto"}} />
+            float: "left",
+            maxWidth:'100%',
+            overflowX: "auto",
+            padding: 15,
+            }}>
+            <img src="/Partenaires/Frites-bonnel.png" style={{height:100,width:"auto",margin:10}}/>
+            <img src ="/Partenaires/POS.png" style={{height:100,width:"auto",margin:10}} />
+            <img src ="/Partenaires/SittiZen.webp" style={{height:100,width:"auto",margin:10}} />
+            <img src ="/Partenaires/temps-instant.jpg" style={{height:100,width:"auto",margin:10}} />
+            <img src ="/Partenaires/ethernis-drone.jpg" style={{height:100,width:"auto",margin:10}} />
+            <img src ="/Partenaires/ludovic-sal.png" style={{height:100,width:"auto",margin:10}} />
+            <img src ="/Partenaires/Prundeblues.png" style={{height:100,width:"auto",margin:10}} />
         </div>
     )
 }
