a1d710c9d07a4f70584b14c2be6c1d1208b121be
Cyrielle Albert <Cyriellealbert@yahoo.fr>
2022-03-29 20:01:12 +0200

    px to rem

--- src/reusable-components/footer.js
+++ src/reusable-components/footer.js
@@ -5,30 +5,30 @@ import { ReactComponent as PhoneSVG } from './../Icons/call-phone.svg'
 //TODO Redirection vers infos
 function Footer1() {
     return (
-        <div style={{backgroundColor: "#0A0E31", width: "100%", height: 250, fontSize:15,color:"white", fontFamily:"Roboto"}}>
-            <div style={{ display: 'flex', flexDirection: "row",  justifyContent: "center", fontFamily: "Roboto", color: "white", fontSize: 15, paddingTop:20 }}>
+        <div style={{backgroundColor: "#0A0E31", width: "100%", height: "15.5rem", fontSize:"1rem",color:"white", fontFamily:"Roboto"}}>
+            <div style={{ display: 'flex', flexDirection: "row",  justifyContent: "center", fontFamily: "Roboto", color: "white", fontSize: "1rem", paddingTop:"1.25rem" }}>
                 <div style={{ width: "33%", justifyContent: "center", display: "flex", flexDirection: "column", alignItems: "center" }}>
                     <img src="/Images/Logo.svg" width="50%" />
                 </div>
                 <div style={{ width: "33%", justifyContent: "center", display: "flex", alignItems: "center" }}>
                     <div style={{ justifyContent: "center", display: "flex", flexDirection: "column" }}>
-                        <span style={{ margin: 10 }}>Le Festival</span>
-                        <span style={{ margin: 10 }}>FAQ</span>
-                        <span style={{ margin: 10 }}>Espace presse</span>
-                        <span style={{ margin: 10 }}>Mentions légales</span>
+                        <span style={{ margin: "0.75rem" }}>Le Festival</span>
+                        <span style={{ margin: "0.75rem" }}>FAQ</span>
+                        <span style={{ margin: "0.75rem" }}>Espace presse</span>
+                        <span style={{ margin: "0.75rem" }}>Mentions légales</span>
                     </div>
                 </div>
                 <div style={{ width: "33%", justifyContent: "center", display: "flex", alignItems: "center", flexDirection: "column" }}>
                     <div style={{ justifyContent: "center", display: "flex", flexDirection: "column" }}>
                         {"Nous suivre : "}
                         <div style={{ justifyContent: "start", display: "flex", flexDirection: "row" }}>
-                            <SocialIcon url="https://www.facebook.com/rives.de.blues.and.co/" bgColor="#FFFFFF" style={{ width: 42, height: 42, margin: "10px 10px 10px 0px" }} />
-                            <SocialIcon url="https://www.instagram.com/rives.de.blues.and.co/" bgColor="#FFFFFF" style={{ width: 42, height: 42, margin: "10px 10px 10px 0px" }} />
+                            <SocialIcon url="https://www.facebook.com/rives.de.blues.and.co/" bgColor="#FFFFFF" style={{ width: "2.625rem", height: "2.625rem", margin: "0.75rem 0.75rem 0.75rem 0rem" }} />
+                            <SocialIcon url="https://www.instagram.com/rives.de.blues.and.co/" bgColor="#FFFFFF" style={{ width: "2.625rem", height: "2.625rem", margin: "0.75rem 0.75rem 0.75rem 0rem" }} />
                         </div>
                         {"Nous contacter : "}
                         <div style={{ justifyContent: "start", display: "flex", flexDirection: "column" }}>
-                            <div style={{ marginTop: 15 }}>
-                                <MailSVG fill="#FFF" width={20} height={20} style={{ marginRight: 10, position: "relative", top: 4 }} />
+                            <div style={{ marginTop: "1rem" }}>
+                                <MailSVG fill="#FFF" width={"1.25rem"} height={"1.25rem"} style={{ marginRight: "0.75rem", position: "relative", top: "0.25rem" }} />
                                 {"Tourdescene@gmail.com"}
                             </div>
                         </div>
