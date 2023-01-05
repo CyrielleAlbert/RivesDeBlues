dc267add7163848dcfd48bb844805b7fdb2d2f70
Cyrielle Albert <cyriellealbert@yahoo.fr>
2023-01-05 20:42:28 +0100

    remove discount

--- src/reusable-components/billetterie.js
+++ src/reusable-components/billetterie.js
@@ -1,39 +1,41 @@
 ﻿import Pass from "./pass";
 import "./style/billetterie.css";
 
-const LINK_BILLET = "https://www.billetweb.fr/rives-de-blues-co1"
+const LINK_BILLET = "https://www.billetweb.fr/rives-de-blues-co1";
 
 function Billetterie() {
   return (
     <>
-      <div style={{textAlign:"center", fontSize: 40 }}>
-              {"Tarifs  PREUM'S    -30% jusqu'au 31 décembre 2022 "}
-      </div>
+      {/* <div style={{ textAlign: "center", fontSize: 40 }}>
+        {"Tarifs  PREUM'S    -30% jusqu'au 31 décembre 2022 "}
+      </div> */}
       <div class="billetterie">
-            <div class="pass-cards">
-              <Pass label="Vendredi soir" prix="17€" reduction="22€" lien={LINK_BILLET}></Pass>
-              <Pass label="Samedi soir" prix="17€" reduction="22€" lien={LINK_BILLET}></Pass>
-              <Pass
-                label="Pass 2 jours"
-                prix="30€"
-                reduction="40€"
-                lien={LINK_BILLET}
-              ></Pass>
-            </div>
-          </div>
-        <div class="info" >
-          <div class="info2">
-            <img
-              src="/Icons/info.svg"
-              width={30}
-              height={30}
-              style={{ marginRight: 20 }}
-            />
-            <div>{
+        <div class="pass-cards">
+          <Pass label="Vendredi soir" prix="22€" lien={LINK_BILLET}></Pass>
+          <Pass label="Samedi soir" prix="22€" lien={LINK_BILLET}></Pass>
+          <Pass
+            label="Pass 2 jours"
+            prix="40€"
+            reduction="44€"
+            lien={LINK_BILLET}
+          ></Pass>
+        </div>
+      </div>
+      <div class="info">
+        <div class="info2">
+          <img
+            src="/Icons/info.svg"
+            width={30}
+            height={30}
+            style={{ marginRight: 20 }}
+          />
+          <div>
+            {
               "La billetterie se fera exclusivement sur la plateforme Billetweb ou sur place."
-            }</div>
+            }
           </div>
-        </div>    
+        </div>
+      </div>
     </>
   );
 }
