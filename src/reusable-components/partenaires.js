bfb8cb9783e5a358e3ef08ce745d315760780f6f
Cyrielle Albert <cyriellealbert@yahoo.fr>
2023-01-07 12:37:58 +0100

    update logos

--- src/reusable-components/partenaires.js
+++ src/reusable-components/partenaires.js
@@ -1,19 +1,66 @@
-﻿function Partenaires(){
-    return(
-        <div style={{
-            display:"flex",
-            flexDirection:"row",
-            justifyContent:"normal",
-            alignItems: 'center',
-            overflowX: "auto",
-            maxWidth:"100%",
-            paddingLeft: 15,
-            }}>
-            <img src ="/Partenaires/SittiZen.webp" style={{height:100,width:"auto",margin:10}} />
-            <img src ="/Partenaires/temps-instant.jpg" style={{height:100,width:"auto",margin:10}} />
-            <img src ="/Partenaires/ludovic-sal.png" style={{height:100,width:"auto",margin:10}} />
-            <img src ="/Partenaires/Prundeblues.png" style={{height:100,width:"auto"}} />
-        </div>
-    )
+﻿function Partenaires() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "normal",
+        alignItems: "center",
+        overflowX: "auto",
+        maxWidth: "100%",
+        paddingLeft: 15,
+        columnGap: 10,
+      }}
+    >
+      <img
+        src="/Partenaires/SittiZen.webp"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/temps-instant.jpg"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/ludovic-sal.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/Prundeblues.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/LOGO_CA_anjou_maine-1.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/Logo_CAPIFRANCE.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/Logo_Jacques_BRIANT.jpg"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/LOGO_MDS_VERTICALE_blanc-1.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/logo_polyphonic-1.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/LOGO_SONS_OF_BLUESjpg.jpeg"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/LOGO_STUDIO_M.png"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+      <img
+        src="/Partenaires/LOGO_TiERCE_VISION_HD.jpg"
+        style={{ height: 100, width: "auto", margin: 10 }}
+      />
+    </div>
+  );
 }
 export default Partenaires;
